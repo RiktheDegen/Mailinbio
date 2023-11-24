2379044013dba5f0b041b5cf4070982a6c59ad40
Srinath Hariharan <shacstudios@gmail.com>
2023-11-24 17:31:39 -0500

    BOT WORKS YAYYYYYYYYYYYYY

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import OpenAI from "openai";
 import axios from 'axios';
+import fs from "fs";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
+
 
 const openai = new OpenAI({
   apiKey: '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',
@@ -17,7 +19,9 @@ const openai = new OpenAI({
 
 app.use(bodyParser.json());
 
+let assistantId;
 let threadId;
+
 
 async function createThread() {
   try {
@@ -40,10 +44,6 @@ const initThread = async () => {
   }
 };
 
-
-
-
-
 // Example endpoint to handle incoming messages
 app.post('/api/messages', async (req, res) => {
   const userMessage = req.body.text; // Assuming your message is sent in the request body
@@ -58,9 +58,10 @@ app.post('/api/messages', async (req, res) => {
     });
 
     const run = await openai.beta.threads.runs.create(threadId, {
-      assistant_id: 'asst_M2KsX8ZbPvYqla6uF7tkKEqH',
+      assistant_id:  assistantId,
       instructions: "Address the user as developer"
     });
+    //'asst_M2KsX8ZbPvYqla6uF7tkKEqH'
 
     let retryCount = 0;
     let success = false;
@@ -102,7 +103,31 @@ app.post('/api/messages', async (req, res) => {
   res.json({ botResponse});
 });
 
+//Begin document upload tests 
+
+const createBot = async () => {
+  console.log('begun function');
+  const fileOne = await openai.files.create({
+    file: fs.createReadStream("./src/assistants API chat/intro.docx"),
+    purpose: "assistants",
+  });
+  const fileTwo = await openai.files.create({
+    file: fs.createReadStream("./src/assistants API chat/tools.docx"),
+    purpose: "assistants",
+  });
+  const assistant = await openai.beta.assistants.create({
+    instructions: "You are a developer support chatbot.Use the given documents to help users with questions regarding the API",
+    model: "gpt-4-1106-preview",
+    tools: [{"type": "retrieval"}],
+    file_ids: [fileOne.id, fileTwo.id]
+  });
+  console.log(assistant.id);
+  assistantId = assistant.id;
+}
+
+
 app.listen(port, async() => {
   console.log(`Server is running on port ${port}`);
+  await createBot();
   await initThread(); // Wait for initThread to complete
 });
