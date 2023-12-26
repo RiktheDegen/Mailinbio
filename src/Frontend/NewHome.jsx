d0cf867f0ac1301ba4573b962ec06ad42dab7ddd
Srinath Hariharan <shacstudios@gmail.com>
2023-12-26 18:39:12 -0500

    new commits

--- src/Frontend/NewHome.jsx
+++ src/Frontend/NewHome.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './NewHome.css'
+import axios from 'axios'
 
 function NewHome() {
     const [activeStep, setActiveStep] = useState(1);
@@ -27,6 +28,23 @@ function NewHome() {
           window.removeEventListener('scroll', handleScroll);
         };
       }, []);
+
+      function submitStripeForm() {
+        // Gather form data or perform any necessary actions
+  
+        // Make an asynchronous POST request to your server using Axios
+        axios.post('https://lorem-ipsum-demo-3115728536ba.herokuapp.com/create-checkout-session', {
+          // Include your form data here
+        })
+        .then(response => {
+          // Handle the response from the server
+          console.log(response.data);
+        })
+        .catch(error => {
+          // Handle errors
+          console.error('There was a problem with the Axios request:', error);
+        });
+      }
     
 
     return (
@@ -182,6 +200,112 @@ function NewHome() {
   </div>
 </div>
 
+{/* Pricing Section */}
+<section className="py-16 " style={{ background: '#1F2937' }} >
+<div className="container mx-auto text-center">
+        <h2 className="text-3xl md:text-4xl font-bold mb-8" style={{ color: '#C2C2C2' }}>Choose Your Plan</h2>
+        <div className="flex justify-center items-center flex-wrap">
+          {/* Pricing cards for Premium, Advanced, and Enterprise plans */}
+          <div className="bg-white p-8 rounded-lg shadow-md mx-4 max-w-md mb-6">
+            <h3 className="text-xl text-gray-600 font-bold mb-2">Premium</h3>
+            <p className="text-3xl font-bold text-gray-800 mb-4">$99/month</p>
+            {/* Gradient Line */}
+            <div className="w-full border-b-2 border-gray-300 my-4"></div>
+            {/* Features */}
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">Advanced Analytics</p>
+            </div>
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">24/7 Customer Support</p>
+            </div>
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">Custom Integrations</p>
+            </div>
+            {/* Buy Button */}
+            <form action="https://lorem-ipsum-demo-3115728536ba.herokuapp.com/create-checkout-session" method="POST">
+              <button className="bg-green-500 text-white py-3 px-8 rounded-lg font-semibold hover:bg-green-400 transition duration-300" type="submit">
+                Choose Plan
+              </button>
+            </form>
+          </div>
+
+          {/* Repeat the above structure for Advanced and Enterprise plans */}
+          <div className="bg-white p-8 rounded-lg shadow-md mx-4 max-w-md mb-6">
+            <h3 className="text-xl text-gray-600 font-bold mb-2">Advanced</h3>
+            <p className="text-3xl font-bold text-gray-800 mb-4">$199/month</p>
+            {/* Gradient Line */}
+            <div className="w-full border-b-2 border-gray-300 my-4"></div>
+            {/* Features */}
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">Advanced Analytics</p>
+            </div>
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">24/7 Customer Support</p>
+            </div>
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">Custom Integrations</p>
+            </div>
+            {/* Buy Button */}
+            <form action="https://your-server.com/create-checkout-session" method="POST">
+              <button className="bg-green-500 text-white py-3 px-8 rounded-lg font-semibold hover:bg-green-400 transition duration-300" type="submit">
+                Choose Plan
+              </button>
+            </form>
+          </div>
+
+          <div className="bg-white p-8 rounded-lg shadow-md mx-4 max-w-md mb-6">
+            <h3 className="text-xl text-gray-600 font-bold mb-2">Enterprise</h3>
+            <p className="text-3xl font-bold text-gray-800 mb-4">Let's Chat</p>
+            {/* Gradient Line */}
+            <div className="w-full border-b-2 border-gray-300 my-4"></div>
+            {/* Features */}
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">Advanced Analytics</p>
+            </div>
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">24/7 Customer Support</p>
+            </div>
+            <div className="flex items-center justify-center text-gray-800 mb-4">
+              <div className="flex items-center justify-center bg-green-500 text-white h-6 w-6 rounded-full mx-2">
+                ✓
+              </div>
+              <p className="mb-2">Custom Integrations</p>
+            </div>
+            {/* Buy Button */}
+            <form action="https://your-server.com/create-checkout-session" method="POST">
+              <button className="bg-green-500 text-white py-3 px-8 rounded-lg font-semibold hover:bg-green-400 transition duration-300" type="submit">
+                Choose Plan
+              </button>
+            </form>
+          </div>
+          {/* You can copy and modify the above structure for the other plans */}
+        </div>
+      </div>
+      </section>
 {/* Let's Get Started Section */}
 
 <div className=" p-8 mt-8 items-center justify-center md:text-left md:text-center ">
@@ -197,6 +321,8 @@ function NewHome() {
 
 
 </section>
+
+
 
 </>
 
