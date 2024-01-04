4bfb4a0d0bc514c21d626190a7abca20cd1c56d4
Srinath Hariharan <shacstudios@gmail.com>
2024-01-04 02:02:02 -0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px', // Custom breakpoint for a minimum width of 320px
+      },
+    },
   },
   plugins: [],
+  
 }
 
